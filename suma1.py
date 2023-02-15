9f4c270c5baca9528529a0c5e7a52fd6ac1147f9
Tabordaaa <tabordajuan1214@gmail.com>
2023-02-15 11:18:12 -0500

    suma y resta en python

--- suma1.py
+++ suma1.py
@@ -5,4 +5,15 @@ num2 = 5
 suma = num + num2
 
 
-print("La suma de los dos numeros es:", suma),
+print("La suma de los dos numeros es:", suma)
+
+
+
+resta = num - num2
+
+
+print("la resta de los dos numeros es:",resta ),
+
+
+
+
