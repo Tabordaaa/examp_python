118502d702a5c4c9af2b68ac0e97fa3f7900844a
Tabordaaa <tabordajuan1214@gmail.com>
2023-02-20 07:43:56 -0500

    dadada

---
+++ vacio.py
@@ -0,0 +1,24 @@
+"""
+variable = 5 
+variable_uno= float(variable)
+variable_dos = str(variable)
+
+
+#sumar_datos= variable+ variable_dos
+print(variable, variable_uno)
+print(variable, variable_uno, variable_dos)
+print(type(variable_dos))
+print(type(variable_uno))
+print(type(variable))
+"""
+
+variable1= "hola"
+variable2= 20
+variable3= 20.8
+variable4= 1j
+variable5= ["cantar","Jugar","Estudiar"] #list 
+variable6= ("cantar","Jugar","Estudiar") #tupla
+variable7= {"cantar","Jugar","Estudiar"} #diccionario
+print(variable5)
+print(variable6)
+print(variable7)
